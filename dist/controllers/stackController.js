7e08d00fa92e137d8742dbf1d2f514dc13c0339e
Opeyemi Ogungbade <Opeyemi.Ogungbade@gmail.com>
2022-08-08 06:41:58 +0100

    protected viewAllStacks

--- dist/controllers/stackController.js
+++ dist/controllers/stackController.js
@@ -2,6 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const asyncHandler = require("express-async-handler");
 const Stacks = require("../models/stack");
+const SuperUser = require("../models/superAdmin.model");
+const Admin = require("../models/admin.model");
+const stacksShield = asyncHandler(async (req, res, next) => {
+    const userID = req.cookies.uID;
+    const superUser = await SuperUser.findOne({ _id: userID });
+    const admin = await Admin.findOne({ _id: userID });
+    if (superUser)
+        next();
+    else if (admin) {
+        res.status(403).json({
+            status: "Failed",
+            message: `Hi ${admin.firstname}, you can only access the stack you have been assigned to`,
+        });
+        return;
+    }
+    else {
+        res.status(403).json({
+            status: "Failed",
+            message: "Access Denied.",
+        });
+        return;
+    }
+});
 const viewAllStacks = asyncHandler(async (req, res) => {
     const allStacks = await Stacks.find({}, { _id: 0, __v: 0 });
     res.status(200).json({
@@ -53,5 +76,5 @@ module.exports = {
     createStack,
     editStack,
     deleteStack,
-    viewAllStacks
+    viewAllStacks,
 };
--- src/controllers/stackController.ts
+++ src/controllers/stackController.ts
@@ -1,9 +1,33 @@
 const asyncHandler = require("express-async-handler");
 const Stacks = require("../models/stack");
+const SuperUser = require("../models/superAdmin.model");
+const Admin = require("../models/admin.model");
 import express, { Request, Response, NextFunction } from "express";
 
+const stacksShield = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userID = req.cookies.uID;
+    const superUser = await SuperUser.findOne({ _id: userID });
+    const admin = await Admin.findOne({ _id: userID });
+    if (superUser) next();
+    else if (admin) {
+      res.status(403).json({
+        status: "Failed",
+        message: `Hi ${admin.firstname}, you can only access the stack you have been assigned to`,
+      });
+      return;
+    } else {
+      res.status(403).json({
+        status: "Failed",
+        message: "Access Denied.",
+      });
+      return;
+    }
+  }
+);
+
 const viewAllStacks = asyncHandler(async (req: Request, res: Response) => {
-  const allStacks = await Stacks.find({}, {_id: 0, __v: 0});
+  const allStacks = await Stacks.find({}, { _id: 0, __v: 0 });
 
   res.status(200).json({
     status: "Success",
@@ -66,5 +90,5 @@ module.exports = {
   createStack,
   editStack,
   deleteStack,
-  viewAllStacks
+  viewAllStacks,
 };
